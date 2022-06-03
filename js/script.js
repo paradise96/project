4ad867070740d8c5643715c4f7cc9bf6df507697
Paradise96 <dimaparadise333gmail.com>
2022-06-03 14:53:47 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,49 @@
+"use strict";
+
+//let number = 5;
+//const leftBorderWidth = 1;
+
+//number = 10;
+//console.log(number);
+//const obj = {
+//name: "Dima",
+//age: 26,
+//isMarried: true,
+//};
+//console.log (obj.isMarried);
+//console.log(obj.name);
+//const arr = ["a", "b", "c"];
+//arr[12] = "1488";
+
+//console.log(arr);
+//const b = 1234;
+// alert('Hello');
+//const result = confirm("Are you here?");
+//console.log(result);
+//const answer = prompt("Vam est 18", "pidar");
+// const answer = +prompt("Vam est 18", "18");
+// console.log(answer + 5);
+// const answers = [];
+// answers[0] = prompt("Хто я?", "");
+// answers[1] = prompt("Ты лох или пидр?", "");
+// answers[2] = prompt("Почему так, Обэмэ?", "");
+// console.log(typeof answers);
+// const category = 'toys'; интерполяция - знак доллара и скобки для продления ссілок или данніх
+// console.log(`https://someurl.com/${category}/5/chlen`);
+// console.log( 4 + ' - cock');
+let incr = 10,
+  decr = 10;
+// ++incr;
+// --decr;
+console.log(++incr);
+console.log(--decr);
+console.log(1 % 2);
+console.log(2 * 4 == 8);
+// console.log( 2*4 === 8); strogoe sravnenie
+// const isChecked = true,
+//       isClose = true;
+// console.log(isChecked && isClose);
+// ! operator otricaniya - pri etom kode vidaet true, a ne false
+const isChecked = false,
+  isClose = false;
+console.log(isChecked || !isClose);
