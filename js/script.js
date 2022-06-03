bdf076c34d2abcf716a3084016e309f81cc5f793
Paradise96 <dimaparadise333gmail.com>
2022-06-03 21:46:53 +0300

    first task

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 "use strict";
 
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+  b = prompt("На сколько оцените его?", ""),
+  c = prompt("Один из последних просмотренных фильмов?", ""),
+  d = prompt("На сколько оцените его?", "");
+
 //let number = 5;
 //const leftBorderWidth = 1;
 
@@ -20,7 +33,7 @@
 // alert('Hello');
 //const result = confirm("Are you here?");
 //console.log(result);
-//const answer = prompt("Vam est 18", "pidar");
+//const answer = prompt("Vam est 18", "pidar"); - dlia togo 4tobu zadat vopros
 // const answer = +prompt("Vam est 18", "18");
 // console.log(answer + 5);
 // const answers = [];
@@ -31,19 +44,28 @@
 // const category = 'toys'; интерполяция - знак доллара и скобки для продления ссілок или данніх
 // console.log(`https://someurl.com/${category}/5/chlen`);
 // console.log( 4 + ' - cock');
-let incr = 10,
-  decr = 10;
+//let incr = 10,
+// decr = 10;
 // ++incr;
 // --decr;
-console.log(++incr);
-console.log(--decr);
-console.log(1 % 2);
-console.log(2 * 4 == 8);
+//console.log(++incr);
+// console.log(--decr);
+// console.log(1 % 2);
+// console.log(2 * 4 == 8);
 // console.log( 2*4 === 8); strogoe sravnenie
 // const isChecked = true,
 //       isClose = true;
 // console.log(isChecked && isClose);
 // ! operator otricaniya - pri etom kode vidaet true, a ne false
-const isChecked = false,
-  isClose = false;
-console.log(isChecked || !isClose);
+// const isChecked = false,
+//   isClose = false;
+// console.log(isChecked || !isClose);
+//git status - proverit faily
+//git add -A - dobavit vse faily
+//git commit -a -m"first commit" - добавление комментария к изменениям файлов. что сделали на этой контрольной точке
+//git log - posmotret kto kogda menial kod
+//git push -u origin master - vykinut fail v internat
+//cd.. - vyiti z papki
+//cd nazvanie papki - voiti v papky
+//git clone http//: git nazvanie papki - ckopirovat rabochii ili drygoi c github
+//git pull - подтянуть те изменения что біли сделаны другими
